8978217e114cdb03fbd4995e0f55cb7c76cca0e8
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-10 21:23:23 +0300

    Completed task

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year % 4) {
+            case 0:
+                if ((year % 100 == 0) && (year % 400 != 0)) {
+                    System.out.println(year + " is not a leap year.");
+                } else {
+                    System.out.println(year + " is a leap year.");
+                }
+                break;
+            default:
+                System.out.println(year + " is not a leap year.");
+                break;
+        }
     }
 }
+
